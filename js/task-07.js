ee154abe49ab2a7c0056b65afda2ace3eb0f0640
KutsaevPavlo <p1ul@ukr.net>
2022-12-25 20:22:44 +0200

    task 08

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,9 @@
 const textInput = document.querySelector('input#font-size-control');
-console.log("🚀 ~ file: task-07.js:2 ~ textInput", textInput)
+
 
 const spanText = document.querySelector('span#text');
 
 textInput.addEventListener("input", (event) => {
-    spanText.style.fontSize = textInput.value + 'px'
+    spanText.style.fontSize = textInput.value + 'px';
+    
     });
