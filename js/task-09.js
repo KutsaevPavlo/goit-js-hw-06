980106a299eeb2832975e9fb33145a4730bc3ca1
KutsaevPavlo <p1ul@ukr.net>
2022-12-25 21:15:43 +0200

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+
+
+const getColor = document.querySelector('.change-color');
+
+const spanColor = document.querySelector('.color');
+
+getColor.addEventListener("click", handleClick);
+
+  
+
+function handleClick () {
+ 
+  spanColor.textContent = getRandomHexColor();
+  
+  bodyEl.style.backgroundColor  = getRandomHexColor();;
+};
+
+
