7d14539e7798a1c53c1d09fe736312750ed331fc
KutsaevPavlo <p1ul@ukr.net>
2022-12-26 22:43:09 +0200

    update task 06

--- js/task-06.js
+++ js/task-06.js
@@ -3,14 +3,22 @@ const textInput = document.querySelector('input#validation-input');
 
 textInput.addEventListener("blur", dataLengthTest);
 
+
+function classManip(remove, add){
+    textInput.classList.remove(remove);
+    textInput.classList.add(add);
+        
+}
+
 function dataLengthTest (event){
 
     if (event.currentTarget.value.length === Number(textInput.dataset.length)) {
-        textInput.classList.remove('invalid');
-        textInput.classList.add('valid');
+        classManip('invalid', 'valid');
+       
     } else if (event.currentTarget.value.length !== Number(textInput.dataset.length)) {
-        textInput.classList.remove('valid');
-        textInput.classList.add('invalid');
+        classManip('valid', 'invalid');
+      
     }}
 ;
+
 
