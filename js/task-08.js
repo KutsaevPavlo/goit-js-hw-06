ef2b9368fdf7b0045448751897728554bdf7db65
KutsaevPavlo <p1ul@ukr.net>
2022-12-26 22:58:03 +0200

    update task 08

--- js/task-08.js
+++ js/task-08.js
@@ -9,7 +9,7 @@ function handleSubmit(event) {
     elements: { email, password }
   } = event.currentTarget;
 
-  if (email.value === "" || password.value === "") {
+  if (email.value.trim() === "" || password.value === "") {
     return console.log("Please fill in all the fields!");
   }
 
